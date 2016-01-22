97f4b4df3c694ef956c8e64b0e9460a1eae67468
Ankit Samarthya <ankitsamar@outlook.com>
2016-01-22 16:39:31 +0530

    Initial boilerplate

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,14 @@
+angular.module('appname', ['ngRoute', 'ngResource', 'templates', 'ui.router'])
+.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'home/_home.html',
+      controller: 'HomeCtrl'
+    });
+
+  $urlRouterProvider.otherwise('/');
+
+  $locationProvider.html5Mode(true);
+}]);
---
+++ app/assets/javascripts/home/homeCtrl.js
@@ -0,0 +1,4 @@
+angular.module('appname')
+.controller('HomeCtrl', [ '$scope', function($scope){
+	$scope.test = "heloo World";
+}]);
